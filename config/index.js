ea89ddcd0480f056d60d0e783d021530dff2fb33
dhika.putra <dhika.putra@visionet.co.id>
2019-11-12 15:20:54 +0700

    fix localhost:8080

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://localhost:8080/home/',
+    assetsPublicPath: 'http://cgo.co.id/',
 
     /**
      * Source Maps
